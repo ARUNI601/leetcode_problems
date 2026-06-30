9f65c5fc9a3d9d4d6535d57c91e1c6ebb67357e8
ARUNI601 <arunithaarulkumar89@gmail.com>
2026-06-30 11:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (14.44%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -3,30 +3,16 @@ class Solution {
         int sum=0;
         int r=mat.length;
         int j=0,k=r-1;
-        if(r%2==0)
+        for(int i=0;i<r;i++)
         {
-            for(int i=0;i<r;i++)
-            {
-                sum=sum+mat[i][j]+mat[i][k];
-                j++;
-                k--;
-            }
+            sum=sum+mat[i][j]+mat[i][k];
+            j++;
+            k--;
         }
-        else
+        if(r%2==1)
         {
-            j=0;
-            k=r-1;
-            for(int i=0;i<r;i++)
-            {
-                sum=sum+mat[i][j];
-                if(i!=k)
-                {
-                    sum=sum+mat[i][k];
-                }
-                j++;
-                k--;
-            }
-        }
+            sum=sum-mat[r/2][r/2];
+        }    
         return sum;
     }
 }
