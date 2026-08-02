e317df0ecad6c2542f735369a59a75a07fe62859
ARUNI601 <arunithaarulkumar89@gmail.com>
2026-08-02 13:31:42 +0530

    Time: 3 ms (86.07%) | Memory: 43.2 MB (58.22%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
+        HashMap<Character,Character> map=new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+        Stack<Character> stack =new Stack<>();
+        for(char ch:s.toCharArray())
+        {
+            if(ch=='(' || ch=='{' || ch=='[')
+            {
                 stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-                if (ch == '}' && top != '{') {
+            }
+            else
+            {
+                if(stack.isEmpty()==true || stack.pop()!=map.get(ch))
+                {
                     return false;
                 }
             }
